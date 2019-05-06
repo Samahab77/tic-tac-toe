3f4bb32f4ac4f206f6d0413b8d3a7ce7ac93ed50
samahab77 <s.bagido@gmail.com>
2019-05-06 15:22:55 +0300

    add restart game and one problem left it is when the plyer add diagonal it does not work

--- main.js
+++ main.js
@@ -20,14 +20,13 @@ $('section #r .box').on('click',function(event){
 
     const position = $(event.target).attr('id')
         if(count%2==0){
-            $(event.target).text(player2);
+            $(event.target).html('<h1>'+player2+'<h1>');
             xoBord[position] = player2
         }else{
-            $(event.target).text(player1);
+            $(event.target).html('<h1>' + player1 + '<h1>');
             xoBord[position] = player1
             
     }
-   
     $(event.target).off('click');
     
     
@@ -58,68 +57,88 @@ const checkWinner = function () {
 //1
 
 if (xoBord !=""){
-
+    $(event.target).off('click');
     if (xoBord[0] === xoBord[1] && xoBord[0] === xoBord[2]  ){
+         blockBoard();
         setTimeout(function(){
             alert(xoBord[1] + " win");
-        },500); 
+        },300); 
     }//r 2
-    else if (xoBord[3] === xoBord[4] && xoBord[3] === xoBord[5] )
+   
+
+        else if (xoBord[3] === xoBord[4] && xoBord[3] === xoBord[5] )
     {
-      setTimeout(function(){
+        blockBoard();
+        setTimeout(function(){
           alert(xoBord[4] + " win");
-        },500); 
+        },300); 
        
      } // 3
     else if (xoBord[6] === xoBord[7] && xoBord[6] === xoBord[8] )
-     {
-       setTimeout(function(){
+    {
+        blockBoard();
+        setTimeout(function(){
            alert(xoBord[7] + " win");
-        },500); 
+        },300); 
        
     } //c 1
     else if (xoBord[0] === xoBord[3] && xoBord[0] === xoBord[6] ) 
-    { 
+    {
+        blockBoard();
        setTimeout(function(){
            alert(xoBord[3] + " win");  
-        },500); 
+        },300); 
    
      }//c2
     else if (xoBord[1] === xoBord[4] && xoBord[1] === xoBord[7] )
-     {
+    {
+        blockBoard();
        setTimeout(function(){
            alert(xoBord[4] + " win");   
-        },500); 
+        },300); 
       
         }//c3
     else if (xoBord[2] === xoBord[5] && xoBord[2] === xoBord[8] ) 
     {
+        blockBoard();
        setTimeout(function(){
            alert(xoBord[5] + " win");   
-        },500); 
+        },300); 
        
         }//d 1
-    else if (xoBord[0] === xoBord[4] && xoBord[0] === xoBord[8] )
-     {
+    else if (xoBord[0] === xoBord[4] && xoBord[0] === xoBord[8])
+    {
+       blockBoard();
        setTimeout(function(){
            alert(xoBord[4] + " win"); 
-        },500); 
+        },300); 
          
         }//d 2
     else if (xoBord[2] === xoBord[4] && xoBord[2] === xoBord[6] ) {
+        blockBoard();
        setTimeout(function(){
            alert(xoBord[4] + " win");  
-        },500); 
+        },300); 
        
         }
          else if (count==9){
+        blockBoard();
              count=1;
         setTimeout(function () {
+            alert("tie")
             alert("game over")
-        }, 500); 
-           
+        }, 300); 
+  
     }
-     
+
     }
-       
+   
     }
+$('button').on('click',function(){
+    location.href = "";
+
+
+})
+const blockBoard = function(){
+    $('section #r .box').off('click');
+}
