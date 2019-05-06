85743994876312dc49c080824393d9eff30105f9
samahab77 <s.bagido@gmail.com>
2019-05-06 12:46:06 +0300

    check winner is don working on no value enterd

--- main.js
+++ main.js
@@ -2,44 +2,124 @@
 let player1='X';
 let player2='O';
 let count=1;
-let =
+let xoBord=[
+    //0, 1, 2
+    //3, 4, 5
+    //6, 7, 8
+    "0","1","2",
+    "3","4","5",
+    "6","7","8"
+]
 
 
-$('section #r #box').on('click',function(event){
-    //
+
+//onclick
+$('section #r .box').on('click',function(event){
 
     // if (count <= 9) {
 
+    const position = $(event.target).attr('id')
         if(count%2==0){
             $(event.target).text(player2);
-        
+            xoBord[position] = player2
         }else{
             $(event.target).text(player1);
+            xoBord[position] = player1
+            
     }
-
+   
     $(event.target).off('click');
-    // $(event.target).append("<h1>"+player1+"</h1>");
-    // $(event.target).append("<h1>" + player2 + "</h1>");
+    
+    
+   
+        checkWinner();
     count++;
-  
-// }else {
-
-//     alert("game over ");
+// }else{
+//     alert("game over ")
 // }
-
-    //  console.log('x');
+    
 })
-// function start (){
-//     if (platyer1)
-// }
- let checkWinner function() {
-     if (box1 === box2 && box1 === box3 || box4 === box5 && box4 === box6 ||
-         box7 === box8 && box7 === box9 || box1 === box4 && box1 === box7 ||
-         box2 === box5 && box2 === box8 || box3 === box6 && box3 === box8 ||
-         box1 === box5 && box1 === box9 || box3 === box5 && box3 === box7   ){
-         alert(player1 || player2 +"win")
-         }else {
-         alert("lsr")  
-         }
-} 
-game1= $()
+
+
+const checkWinner = function () {
+
+    // $(event.target).off('click');
+    
+    // if (xoBord[0] === xoBord[1] && xoBord[0] === xoBord[2] || xoBord[3] === xoBord[4] && xoBord[3] === xoBord[5] ||
+    //     xoBord[6] === xoBord[7] && xoBord[6] === xoBord[8] || xoBord[0] === xoBord[3] && xoBord[0] === xoBord[6] ||
+    //     xoBord[1] === xoBord[4] && xoBord[1] === xoBord[7] || xoBord[2] === xoBord[5] && xoBord[2] === xoBord[8] ||
+    //     xoBord[0] === xoBord[4] && xoBord[0] === xoBord[8] || xoBord[2] === xoBord[4] && xoBord[2] === xoBord[6]) 
+    //     {
+    //         alert( "win")
+    //     }
+    // }
+
+// game1 = $(event.target).text(player2);
+//1
+
+if (xoBord !=""){
+
+    if (xoBord[0] === xoBord[1] && xoBord[0] === xoBord[2]  ){
+        setTimeout(function(){
+            alert(xoBord[1] + " win");
+        },500); 
+    }//r 2
+    else if (xoBord[3] === xoBord[4] && xoBord[3] === xoBord[5] )
+    {
+      setTimeout(function(){
+          alert(xoBord[4] + " win");
+        },500); 
+       
+     } // 3
+    else if (xoBord[6] === xoBord[7] && xoBord[6] === xoBord[8] )
+     {
+       setTimeout(function(){
+           alert(xoBord[7] + " win");
+        },500); 
+       
+    } //c 1
+    else if (xoBord[0] === xoBord[3] && xoBord[0] === xoBord[6] ) 
+    { 
+       setTimeout(function(){
+           alert(xoBord[3] + " win");  
+        },500); 
+   
+     }//c2
+    else if (xoBord[1] === xoBord[4] && xoBord[1] === xoBord[7] )
+     {
+       setTimeout(function(){
+           alert(xoBord[4] + " win");   
+        },500); 
+      
+        }//c3
+    else if (xoBord[2] === xoBord[5] && xoBord[2] === xoBord[8] ) 
+    {
+       setTimeout(function(){
+           alert(xoBord[5] + " win");   
+        },500); 
+       
+        }//d 1
+    else if (xoBord[0] === xoBord[4] && xoBord[0] === xoBord[8] )
+     {
+       setTimeout(function(){
+           alert(xoBord[4] + " win"); 
+        },500); 
+         
+        }//d 2
+    else if (xoBord[2] === xoBord[4] && xoBord[2] === xoBord[6] ) {
+       setTimeout(function(){
+           alert(xoBord[4] + " win");  
+        },500); 
+       
+        }
+         else if (count==9){
+             count=1;
+        setTimeout(function () {
+            alert("game over")
+        }, 500); 
+           
+    }
+     
+    }
+       
+    }
